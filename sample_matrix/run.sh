01cb5ce64e293add1a17260d97dc269fb09876bc
Wang Meng <kiranosora@gmail.com>
2018-09-04 15:47:04 +0800

    Refresh

--- sample_matrix/run.sh
+++
@@ -1,5 +0,0 @@
-make clean
-make
-./CalEff 1 2>&1 |tee logs/log
-./Correc 1 2>&1 |tee logs/cLog
-root -l draw.cxx
--- sample_matrix/test.sh
+++
@@ -1,2 +0,0 @@
-g++ `root-config --cflags --libs` test.cxx -o TEST
-./TEST 1 2>&1|tee logs/testLog
